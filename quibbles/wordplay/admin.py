6522a37ec48231adc4fe05b3f4c5de899eae3183
Eduardo <eduardo.cxcorreio@gmail.com>
2018-03-23 21:45:02 +0100

    Customized Admin Site with a ModelAdmin class.

--- quibbles/wordplay/admin.py
+++ quibbles/wordplay/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Quibble
+
+# admin.site.register(Quibble)
+
+
+@admin.register(Quibble)
+class QuibbleAdmin(admin.ModelAdmin):
+
+    def user_name(self, obj):
+        return obj.user.username
+
+    list_display = ('id', 'text', 'category', 'status', 'user_name', 'created', 'modified')
+    list_editable = ('status',)
--- quibbles/wordplay/models.py
+++ quibbles/wordplay/models.py
@@ -1,8 +1,17 @@
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
+
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+QUIBBLE_STATUS_CHOICES = (
+    ('C', 'Created'),
+    ('M', 'Modified'),
+)
 
+
+@python_2_unicode_compatible
 class Quibble(models.Model):
     text = models.TextField()
     category = models.CharField(max_length=50)
@@ -12,4 +21,7 @@ class Quibble(models.Model):
 
     user = models.ForeignKey(User, related_name="quibbles_user_id", on_delete=models.CASCADE)
 
-    status = models.CharField(max_length=1, default='C')
+    status = models.CharField(max_length=1, default='C', choices=QUIBBLE_STATUS_CHOICES)
+
+    def __str__(self):
+        return f'{self.text} - {self.category} - {self.user.username}'
